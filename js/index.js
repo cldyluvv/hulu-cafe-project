a283d9b57777c01931de23973f317b78fed06422
alianatasha2508 <alianatasha2508@gmail.com>
2026-07-08 01:48:16 +0800

    tambah background music

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+        //HERO CAROUSEL
     const heroCarousel = document.querySelector("#heroCarousel");
 
     new bootstrap.Carousel(heroCarousel, {
@@ -8,6 +9,42 @@ document.addEventListener("DOMContentLoaded", function () {
         pause: false,     // tak berhenti bila mouse atas gambar
         wrap: true,       // lepas gambar terakhir, sambung gambar pertama
         touch: true       // boleh swipe dekat phone
+    });
+
+    // BACKGROUND MUSIC
+
+ const music = document.getElementById("bgMusic");
+ document.addEventListener("click", () => {
+    music.play();
+}, { once: true });
+    const btn = document.getElementById("musicToggle");
+    const icon = btn.querySelector("i");
+
+    music.volume = 0.3;
+
+    // Browser biasanya block autoplay
+    document.body.addEventListener("click", function(){
+
+        if(music.paused){
+            music.play().catch(()=>{});
+        }
+
+    }, { once:true });
+
+    btn.addEventListener("click", function(){
+
+        if(music.paused){
+
+            music.play();
+            icon.className = "bi bi-volume-up-fill";
+
+        }else{
+
+            music.pause();
+            icon.className = "bi bi-volume-mute-fill";
+
+        }
+
     });
 
 });
